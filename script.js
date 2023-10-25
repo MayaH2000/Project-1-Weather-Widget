b2337bd640d3afbd3a2d02e42de0ffb02a7915da
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2023-10-25 13:18:28 -0500

    created variables, added ApiKey, added findCity function

---
+++ script.js
@@ -0,0 +1,36 @@
+var JwFaRzvC1eoVIJydxjUJfIHuj5Q2gNGh
+
+
+//Declare a variable to store the searched city
+var city="";
+// variable declaration
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+
+// searches the city to see if it exists in the entries from the storage
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+
+//Set up the API key
+var APIKey="JwFaRzvC1eoVIJydxjUJfIHuj5Q2gNGh";
+// Display the curent and future weather to the user after grabing the city form the input text box.
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
